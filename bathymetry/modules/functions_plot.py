50c5dfba0f254c1f069d094017a2cf8731e384b7
Idalia <imachuca@eos.ubc.ca>
2017-01-03 12:57:59 -0800

    nothing (tried changing masking but haven't done right)

--- bathymetry/modules/functions_plot.py
+++ bathymetry/modules/functions_plot.py
@@ -141,7 +141,7 @@ def plot_region(fig, ax, x_region, y_region, z_region):
 #--------------------------------------------------------------------------------------
 
 def mask_output(var):
-    var_m = np.ma.masked_values(var, 0)
+    var_m = np.ma.masked_values(var, 0.0000)
     return var_m
 
 #--------------------------------------------------------------------------------------
